d8e80b1b923a32c13249e36e8f30378f9bee902b
Ana Livia Rosa Couto <ana.couto4415@gmail.com>
2024-06-14 13:59:27 -0300

    conexao banco de dados

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, redirect, render_template, session
 from hashlib import sha256
 from conexao import Conexao
+from usuario import BancoDeDados
+import random
 
 app = Flask(__name__)
 
@@ -39,6 +41,56 @@ def ver_carrinho():
     mybd.close()
 
     return render_template("carrinho.html", carrinho_detalhes=carrinho_detalhes)
+
+
+
+@app.route("/cadastro")
+def pagina_cadastro():
+    return render_template("cadastro.html")
+
+@app.route("/cadastrar", methods=["POST"])
+def cadastrar():
+    nome = request.form["nome"]
+    email = request.form["email"]
+    senha = request.form["senha"]
+
+    # Aqui você pode chamar o método para cadastrar o usuário no banco de dados
+    banco = BancoDeDados()
+    banco.cadastrar_usuario(email, senha, nome)
+
+    # Mensagem de sucesso
+    mensagem = "Cadastro realizado com sucesso"
+
+    # Renderiza a página de cadastro com a mensagem de sucesso
+    return render_template("cadastro.html", mensagem=mensagem)
+
+
+
+    
+
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.form["email"]
+    senha = request.form["senha"]
+
+    banco = BancoDeDados()
+    usuario = banco.verificar_login(email, senha)
+
+    if usuario:
+        mensagem = "Login bem-sucedido!"
+        return render_template("index.html", mensagem=mensagem)
+    else:
+        mensagem = "Email ou senha incorretos. Tente novamente."
+        return render_template("index.html", mensagem=mensagem)
+
+@app.route('/emails_cadastrados')
+def emails_cadastrados():
+    banco = BancoDeDados()
+    emails = banco.listar_emails()
+    return render_template('emails_cadastrados.html', emails=emails)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 if __name__ == "__main__":
