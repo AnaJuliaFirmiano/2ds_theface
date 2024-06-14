fb755608b008e554653a61153f0d16a6ad9dc220
Ana Firmiano <ana.firmiano4353@gmail.com>
2024-06-14 13:22:12 -0300

    Correções Carrinho

--- conexao.py
+++ conexao.py
@@ -5,7 +5,7 @@ class Conexao():
     def conectar():
         mybd = mysql.connector.connect(
             host = "127.0.0.1",
-            port = 3307,
+            port = 3306,
             user = "root",
             password = "root",
             database = "theface"
